6c758777b222770a66de66ac97243ffd06d96248
Sergey Fomin <gray7410@yandex.ru>
2022-06-24 10:52:59 +0500

    Create reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { initiateStore } from "./store/store";
-import * as actions from "./store/actions";
+import configureStore from "./store/store";
+import { taskCompleted, titleChanged, taskDeleted } from "./store/task";
 
-const store = initiateStore();
+const store = configureStore();
 
 const App = (params) => {
   const [state, setState] = useState(store.getState());
@@ -15,10 +15,13 @@ const App = (params) => {
   }, []);
 
   const completeTask = (taskId) => {
-    store.dispatch(actions.taskCompleted(taskId));
+    store.dispatch(taskCompleted(taskId));
   };
   const changeTitle = (taskId) => {
-    store.dispatch(actions.titleChanged(taskId));
+    store.dispatch(titleChanged(taskId));
+  };
+  const deleteTask = (taskId) => {
+    store.dispatch(taskDeleted(taskId));
   };
   return (
     <>
@@ -28,9 +31,10 @@ const App = (params) => {
         {state.map((el) => (
           <li key={el.id}>
             <p>{el.title}</p>
-            <p>{`${el.completed}`}</p>
+            <p>Completed: {`${el.completed}`}</p>
             <button onClick={() => completeTask(el.id)}>Complete</button>
             <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
             <hr />
           </li>
         ))}
